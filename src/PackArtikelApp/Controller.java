a434709f683623ae022bc104580ffe52ef5b8722
aedign <aedign@icloud.com>
2019-12-20 16:55:46 +0000

    HashMap replaced parsing file to find a word

--- src/PackArtikelApp/Controller.java
+++ src/PackArtikelApp/Controller.java
@@ -1,24 +1,69 @@
 package PackArtikelApp;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Controller extends Application {
 
+    @FXML
+    private TextField articleTextField;
+
+    @FXML
+    private TextField wordTextField;
+
+    private NounFinder theNounFinder = new NounFinder();
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainScreenFXML.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("ArtikelApp");
+        primaryStage.setScene(new Scene(root, 350, 200));
+        theNounFinder.loadHashMap();
         primaryStage.show();
     }
 
+    @FXML
+    private void setGoButton(ActionEvent e){
 
-    public static void main(String[] args) {
-        launch(args);
+        articleTextField.setText(theNounFinder.getArticle(wordTextField.getText()));
+        formatText();
+    };
+
+    private void formatText(){
+        String formattedInput = wordTextField.getText().substring(0, 1).toUpperCase() +
+                wordTextField.getText().substring(1).toLowerCase();
+        wordTextField.setText(formattedInput);
     }
 
+    @FXML
+    private void setClearButton(ActionEvent e){
+        articleTextField.clear();
+        wordTextField.clear();
+    }
+
+    @FXML
+    private void writeCharacter(ActionEvent e){
+
+        MenuItem mi = (MenuItem) e.getSource();
+        String character = mi.getText();
+
+        if(character.equalsIgnoreCase("ä"))
+            wordTextField.setText(wordTextField.getText() + "ä");
+        else if(character.equalsIgnoreCase("ö"))
+            wordTextField.setText(wordTextField.getText() + "ö");
+        else if(character.equalsIgnoreCase("ü"))
+            wordTextField.setText(wordTextField.getText() + "ü");
+    }
+
+
 }
+
+
--- src/PackArtikelApp/NounFinder.java
+++ src/PackArtikelApp/NounFinder.java
@@ -16,24 +16,22 @@ import java.util.*;
 
 public class NounFinder {
     
-    protected static Scanner fileScanner;
-    public static String parsedLine;
-    public static String wordToPrint;
-    public static HashMap<String, String> wordMap = new HashMap<>();
+    private static Scanner fileScanner;
+    private static String parsedLine;
+    public static String articleToPrint;
+    private static HashMap<String, String> wordMap = new HashMap<>();
 
+    protected static void loadHashMap(){
 
-    public static void loadHashMap(){
-
-        openFile("Nouns.txt");
-
-            while (fileScanner.hasNext()) {
-                parsedLine = fileScanner.nextLine();
-                wordMap.put(findWordInLine(fileScanner.nextLine()), fileScanner.nextLine());
-            }
+        openFile();
+        while (fileScanner.hasNext()) {
+            parsedLine = fileScanner.nextLine();
+            wordMap.put(findWordInLine(parsedLine), parsedLine);
+        }
         fileScanner.close();
     }
 
-    public static String findWordInLine(String line){
+    protected static String findWordInLine(String line){
 
         String comma = "";
         int i = 0;
@@ -45,86 +43,46 @@ public class NounFinder {
         return line.substring(0,i-1);
     }
 
-    static void openFile(String fileName){
+   private static void openFile(){
 
       try {
-         fileScanner = new Scanner(new File(fileName));
+         fileScanner = new Scanner(new File("Nouns.txt"));
          }
 
      catch(Exception e){
            System.out.println("File not found");
            }
      }
-    /*
-     static void readFile(String wordtoFind) {
 
-      // split tasks into another two methods and call here
-     int count = 0;
+    private static String searchArticle(String findWordArticle) {
 
-        long time1 = System.currentTimeMillis();
-
-            
-        try{
-		    while(fileScanner.hasNext()) {
-
-            parsedLine = fileScanner.next();
-
-            if (parsedLine.endsWith(",")) {
-
-                if (parsedLine.substring(0, wordtoFind.length()).equalsIgnoreCase(wordtoFind)
-                        &&
-                        parsedLine.substring(0, wordtoFind.length() + 1).endsWith(",")) {
-
-                    //   wordToPrint = lineSearch.replace(",", "");
-
-                    wordToPrint = parsedLine.substring(0, wordtoFind.length());
-
-                    System.out.println(searchArticle(parsedLine) + " " + wordToPrint);
-                    return;
-                }
-            } else {
-                while (!parsedLine.endsWith(","))
-                    parsedLine += " " + fileScanner.next();
-
-                if (parsedLine.substring(0, wordtoFind.length()).equalsIgnoreCase(wordtoFind)
-                        &&
-                        parsedLine.substring(0, wordtoFind.length() + 1).endsWith(",")) {
-
-                    // wordToPrint = lineSearch.replace(",", "");
-
-                    wordToPrint = parsedLine.substring(0, wordtoFind.length());
-
-                    System.out.println(searchArticle(parsedLine) + " " + wordToPrint);
-                    return;
-                }
-            }
-        }
-		System.out.println("Word not found.");
-    }
-    catch(Exception e){
-        System.out.println("Exception: Word not in file or entered incorrectly.");
-    }
-    fileScanner.close();
-}
-*/
-
-    static String searchArticle(String findWordArticle){
-
-        if(findWordArticle.contains(",f,"))
+        if (findWordArticle.contains(",f,"))
             return "die";
         else if (findWordArticle.contains(",m,"))
             return "der";
-        else if(findWordArticle.contains(",n,"))
+        else if (findWordArticle.contains(",n,"))
             return "das";
         else
-            return ("Word has no article");
+            return ("N/A");
     }
 
-        
+    protected String getArticle(String unformattedInput){
+
+        if(!unformattedInput.equalsIgnoreCase("")) {
+            String formattedInput = unformattedInput.substring(0, 1).toUpperCase() +
+                    unformattedInput.substring(1).toLowerCase();
+
+            return (wordMap.get(formattedInput) != null) ?
+                    searchArticle(wordMap.get(formattedInput)): "N/A";
+        }
+        else
+            return "N/A";
+    }
+
     public static void main(String[] args){
 
         loadHashMap();
-        String unformattedInput = "1. Johannesbrief";
+        String unformattedInput = "paul smith";
 
         String formattedInput = unformattedInput.substring(0,1).toUpperCase() +
                 unformattedInput.substring(1).toLowerCase();
@@ -133,21 +91,19 @@ public class NounFinder {
 
         String lowerCaseInput = unformattedInput.toLowerCase();
 
+        if(wordMap.get(unformattedInput) != null)
+            System.out.println(wordMap.get(unformattedInput));
 
-
-
-
-    if(wordMap.get(unformattedInput) != null)
-        System.out.println(wordMap.get(unformattedInput));
+        System.out.println(searchArticle(wordMap.get(unformattedInput)));
 
         if(wordMap.get(formattedInput) != null)
             System.out.println(wordMap.get(formattedInput));
 
-    if(wordMap.get(upperCaseInput) != null)
-        System.out.println(wordMap.get(upperCaseInput));
+        if(wordMap.get(upperCaseInput) != null)
+            System.out.println(wordMap.get(upperCaseInput));
 
-    if(wordMap.get(lowerCaseInput) != null)
-        System.out.println(wordMap.get(lowerCaseInput));
+        if(wordMap.get(lowerCaseInput) != null)
+            System.out.println(wordMap.get(lowerCaseInput));
 
     // "viertelscheffel" cannot be found. error in file. WHY?
 
